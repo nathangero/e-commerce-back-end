d24ebdcc7ca1bff643a3c1fd60df2faacb3dfbb4
nathangero <nathangero@yahoo.com>
2023-11-01 15:44:19 -0700

    implement .put '/:id'

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,30 +29,42 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // create a new category
     try {
-      
+        const data = await Category.create(req.body);
+        res.status(200).json(data);
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     // update a category by its `id` value
     try {
-      
+        const data = await Category.update(req.body, {
+            where: {
+              id: req.params.id,
+            }
+        });
+
+        if (data[0]) {
+            res.status(200).json({ message: 'Changed category name' });
+        } else {
+            res.status(404).json({ message: "Category doesn't exist" });
+        }
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     // delete a category by its `id` value
     try {
       
+        res.status(200).json(data);
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
 
