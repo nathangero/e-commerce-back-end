22b41f9cebc87f719a3852fd40b1ee6d4890c0fb
nathangero <nathangero@yahoo.com>
2023-11-01 16:26:12 -0700

    fix belongsToMany; implement get '/'

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,34 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+    // find all tags
+    // be sure to include its associated Product data
+    try {
+        const data = await Tag.findAll({
+            include: [{ model: Product}]
+        })
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+    // create a new tag
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+    // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+    // delete on tag by its `id` value
 });
 
 module.exports = router;
